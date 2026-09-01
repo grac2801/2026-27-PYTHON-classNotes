94e163b186b2b20e127a2edfd2a3821f483fbf64
grac2801 <oscar.gracias@icloud.com>
2026-09-01 11:59:29 -0700

    U1_L7 data types

---
+++ unit001_basics/U1_L7_dataTypes.py
@@ -0,0 +1,71 @@
+'''
+Created on Sept 1, 2026
+
+@author: ogracias
+'''
+# Adding 2 numbers [incorrectly]
+# num1 = int(input('Enter the first number: '))
+# num2 = int(input('Enter the second number: '))
+# result = num1 + num2
+# print('The result is: ', result)
+
+
+# Entering a number
+# number = int(input('Enter an integer: '))
+# print('You entered a ' + str(number))
+# print('You entered a', number)
+
+
+# combining inputs
+# color = input('What is your favorite color? ')
+# num = int(input('Enter your lucky number: '))
+# print(color + ' is my favorite color too!', end='')
+# print('I have ' + str(num) + ' dogs at my house.')
+
+
+# Decimal numbers
+firstDecimal = 5.545454
+secondDecimal = 7.02154
+thirdDecimal = float(input('Enter a third decimal value: '))
+total = firstDecimal + secondDecimal + thirdDecimal
+print('My total is ' + str(total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
