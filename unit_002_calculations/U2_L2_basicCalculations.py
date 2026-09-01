5056f07f3ab2134402d29d3d990e4582290a0a9f
grac2801 <oscar.gracias@icloud.com>
2026-09-01 12:55:46 -0700

    U2_L2 basic calculations part 1

---
+++ unit_002_calculations/U2_L2_basicCalculations.py
@@ -0,0 +1,37 @@
+'''
+Created on Sep 1, 2026
+
+@author: ogracias
+'''
+# x = 37
+# y = 14
+# print("The answer is " + str(x + y))
+
+# Fahrenheit to Celsius
+# f = float(input('Enter temperature in Fahrenheit: '))
+# c = (f - 32) * (5/9)
+# print('The Celsius Temperature is = ' + str(c))
+
+# Celsius to Fahrenheit 
+celsius = float(input('Enter temperature in Celsius: '))
+f = (celsius * (9/5) + 32)
+print('The Fahrenheit Temperature is = ' + str(f))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
