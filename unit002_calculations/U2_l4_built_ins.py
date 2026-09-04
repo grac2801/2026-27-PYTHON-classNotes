fad1e43bc0505997ce093a7e2d7b99358a49acc9
grac2801 <oscar.gracias@icloud.com>
2026-09-04 12:09:09 -0700

    U2_L4 built ins

---
+++ unit002_calculations/U2_l4_built_ins.py
@@ -0,0 +1,63 @@
+'''
+Created on Sep 4, 2026
+
+@author: ogracias
+'''
+
+import math
+
+print(4, 5, 2, 9, 23, 54, -3, -2)
+# find the smallest
+print(min(4, 5, 2, 9, 23, 54, -3, -2))
+# find the largest
+print(max(4, 5, 2, 9, 23, 54, -3, -2))
+
+
+print('****************')
+print("min and max algorithm")
+print('****************')
+a = int(input('Enter a number: '))
+b = int(input('Enter a number: '))
+c = int(input('Enter a number: '))
+
+print('Biggest value: ' + str(max(a, b, c)))
+print('smallest value: ' + str(min(a, b, c)))
+
+
+print(344 / 3.5) # division
+print(int(344 // 3.5)) # integer division [only int portion is displayed]
+
+print(pow(16, (1/2)))
+
+
+print(math.sqrt(16))
+
+print(math.cos(100))
+
+print(pow(2, 3))
+answer = math.pow(2.2, 3)
+print(f"The answer is: {answer:.2f}")
+
+print(2 ** 3) #[2 to the power of 3]
+
+print(math.fabs(-587.52)) #[absolute value]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
