cf84246ec31d6fec59860d193c2d6440619a4e5a
grac2801 <oscar.gracias@icloud.com>
2026-09-10 12:57:58 -0700

    U3_L2 simple ifs and testAndGrade project

--- unit003_Selection/U3_L2_simple_ifs.py
+++ unit003_Selection/U3_L2_simple_ifs.py
@@ -26,14 +26,69 @@ print('\n\n')
 print('==================================================')
 print('TOPIC: -->', '3. odd or even' )
 print('==================================================')
-number = int(input('Enter a number: '))
+# number = int(input('Enter a number: '))
 # check if even or odd
-if number % 2 == 0:
-    print(str(number) + ' is even')
-if number % 2 == 1:
-    print(str(number) + ' is odd')
-if number % 2 == 0 or number % 3 == 0:
-    print(str(number) + ' is divisible by 2 or 3')
+# if number % 2 == 0:
+#     print(str(number) + ' is even')
+# if number % 2 == 1:
+#     print(str(number) + ' is odd')
+# if number % 2 == 0 or number % 3 == 0:
+#     print(str(number) + ' is divisible by 2 or 3')
+
+print('\n\n')
+print('==================================================')
+print('TOPIC: -->', 'syntax is grammar of coding' )
+print('==================================================')
+# word = input('Enter the secret word: ')
+# if word == 'python':
+#     print('correct')
+
+
+
+print('\n\n')
+print('==================================================')
+print('TOPIC: -->', 'Relational operations' )
+print('==================================================')
+print('==', 'equal to')
+print('!=', 'not equal to')
+print('<', 'is less than')
+print('>', 'is greater than')
+print('<=', 'is less than or equal to')
+print('>=', 'is greater than or equal to')
+print('in', 'membership')
+
+
+print('\n\n')
+print('==================================================')
+print('TOPIC: -->', 'student activity' )
+print('==================================================')
+print('write a program to input an int number and test whether')
+print('this number is even and if between 50 inclusive and 75 ')
+print('not inclusive (exclusive)')
+
+a = int(input('Enter a value: '))
+if a % 2 == 0:
+    print('Number is even')
+if a >= 50 and a < 75:
+    print('Number is between 50 and 75 not inclusive')
+print('You entered number: ' + str(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ unit003_Selection/testAndGrades.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 10, 2026
+
+@author: ogracias
+'''
+#===========================================================================
+#student activity
+#
+# Make a program which asks students in elementary school
+# 5 multiple choice questions
+# 1-shows the total of questions correctly answered
+# 2-Provides a percent grade
+# 3-Make two of the questions have 2 answers providing
+#   1/2 point for every correct choice.
+#===========================================================================
+
+
+
+
+
+
